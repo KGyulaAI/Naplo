76b774aed2f1f01f09f3b7e7e3ecb3a81be110b8
KGyulaAI <71291570+KGyulaAI@users.noreply.github.com>
2023-03-10 15:26:20 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string fajlNev = "naplo.txt";
+        List<Osztalyzat> jegyek = new List<Osztalyzat>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OsztalyzatokBetoltese(string fajlNev)
+        {
+            jegyek.Clear();
+            StreamReader sr = new StreamReader(fajlNev);
+            while (!sr.EndOfStream)
+            {
+                string[] mezok = sr.ReadLine().Split(";");
+                Osztalyzat ujJegy = new Osztalyzat(mezok[0], mezok[1], mezok[2], int.Parse(mezok[3]));
+                jegyek.Add(ujJegy);
+            }
+            sr.Close();
+            dgJegyek.ItemsSource = jegyek;
+            MessageBox.Show("Az állomány beolvasása befejeződött!");
+        }
+
+        private void btnRogzit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBetoltes_Click(object sender, RoutedEventArgs e)
+        {
+            OsztalyzatokBetoltese(fajlNev);
+        }
+    }
+}
